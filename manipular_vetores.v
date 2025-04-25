caa6fdc6b32e22852575576698e6f2c7bff7a260
Luiz Felipe <luiz.silva@smart.agr.br>
2025-04-25 05:19:17 -0300

    update l06

--- manipular_vetores.v
+++ manipular_vetores.v
@@ -2,6 +2,7 @@ module manipular_vetores(
     input [31:0] entrada,
     output [31:0] saida );
 
-    // insira seu código aqui
+    // inverter a ordem dos bytes para trocar a endianess
+    assign saida = {entrada[7:0], entrada[15:8], entrada[23:16], entrada[31:24]};
 
 endmodule
